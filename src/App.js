3fefbdf2be39e7962026e8ce9292b75601d6b6fe
Jia Hui Liou <30471836+jiahui816@users.noreply.github.com>
2021-01-26 19:38:29 +1100

    added item component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,100 @@
 import "./App.css";
 import Header from "./components/Header";
 
+import Accessories from "./assets/Desktop-Accessories.jpg";
+import ModelS from "./assets/Desktop-ModelS.jpeg";
+import Model3 from "./assets/Desktop-Model3.jpeg";
+import ModelX from "./assets/Desktop-ModelX.jpeg";
+import ModelY from "./assets/Desktop-ModelY.jpeg";
+import SolarPanels from "./assets/Desktop-SolarPanels.jpeg";
+import SolarRoof from "./assets/Desktop-SolarRoof.jpeg";
+import Item from "./components/Item";
+
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="app__itemContainer">
+        <Item
+          title="Lowest Cost Solar Panels in America"
+          desc="Money-back guarantee"
+          descLink=""
+          backgroundImg={SolarPanels}
+          leftBtnTxt="ORDER NOW"
+          leftBtnLink=""
+          rightBtnTxt="LEARN MORE"
+          rightBtnLink=""
+          twoButtons="true"
+          first
+        />
+        <Item
+          title="Lowest Cost Solar Panels in America"
+          desc="Money-back guarantee"
+          descLink=""
+          backgroundImg={SolarPanels}
+          leftBtnTxt="ORDER NOW"
+          leftBtnLink=""
+          rightBtnTxt="LEARN MORE"
+          rightBtnLink=""
+          twoButtons="true"
+          first
+        />
+        <Item
+          title="Model S"
+          desc="$70,000"
+          descLink=""
+          backgroundImg={ModelS}
+          leftBtnTxt="CUSTOM ORDER"
+          leftBtnLink=""
+          rightBtnTxt="LEARN MORE"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+        <Item
+          title="Model 3"
+          desc="$73,000"
+          descLink=""
+          backgroundImg={Model3}
+          leftBtnTxt="CUSTOM ORDER"
+          leftBtnLink=""
+          rightBtnTxt="LEARN MORE"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+        <Item
+          title="Model X"
+          desc="$79,000"
+          descLink=""
+          backgroundImg={ModelX}
+          leftBtnTxt="CUSTOM ORDER"
+          leftBtnLink=""
+          rightBtnTxt="LEARN MORE"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+        <Item
+          title="Model Y"
+          desc="$99,000"
+          descLink=""
+          backgroundImg={ModelY}
+          leftBtnTxt="CUSTOM ORDER"
+          leftBtnLink=""
+          rightBtnTxt="LEARN MORE"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+        <Item
+          title="Solar Roof"
+          desc="$99,000"
+          descLink=""
+          backgroundImg={SolarRoof}
+          leftBtnTxt="CUSTOM ORDER"
+          leftBtnLink=""
+          rightBtnTxt="LEARN MORE"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+      </div>
     </div>
   );
 }
