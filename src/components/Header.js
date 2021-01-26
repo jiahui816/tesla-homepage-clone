ca887d6f7ef829f29ec1c2426f481e46cb5b5bc7
Jia Hui Liou <30471836+jiahui816@users.noreply.github.com>
2021-01-26 17:16:59 +1100

    header

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Header.css";
+import TeslaLogo from "../assets/teslaLogoSmall.svg";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__logo">
+        <img src={TeslaLogo} alt="TeslaLogo" />
+      </div>
+      <div className="header__center">
+        <p>Model S</p>
+        <p>Model 3</p>
+        <p>Model X</p>
+        <p>Model Y</p>
+        <p>Cybertruck</p>
+        <p>Powerwall</p>
+      </div>
+      <div className="header__right">
+        <p>Shop</p>
+        <p>Tesla Account</p>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
